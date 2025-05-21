fd6987922744019cddab45e0f87adfd0948c0849
Tobia Ochsner <tobia.ochsner@hotmail.com>
2025-05-21 15:51:28 +0200

    Improve simple benchmarking in main.

--- src/lib.rs
+++ src/lib.rs
@@ -15,16 +15,11 @@ mod trees_parser_tests;
 pub fn parse_file(path: &str) {
     let contents = fs::read_to_string(path).expect("Should have been able to read the file");
 
-    println!("Lexing...");
     let lexer = Lexer::new(&contents);
     let tokens = Tokens::new(&lexer);
 
-    println!("Parsing...");
     let mut parser = Parser::new(tokens);
     let result = parser.parse().unwrap();
-
-    println!("Parsing done.");
-    println!("OK: {}", result.blocks.len());
 
     for block in result.blocks {
         if let nexus::NexusBlock::TreesBlock(_, trees) = block {
